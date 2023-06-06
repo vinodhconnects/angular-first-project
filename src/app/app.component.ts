25060dba4664faa950cdc3643201b91b7e486d4c
vinodhconnects <vinodhconnects@gmail.com>
2023-06-06 11:22:53 +0530

    ngstyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,10 @@ export  class AppComponent {
   themes:string[] = ["dark" , "light", " blue " ," darkred" ]
   themeStyles:ThemeProps[]= [
      {backgroundColor:"#2E4053",color: "whitesmoke"},
-     {backgroundColor:"#2E4053", color: "#2E4053"},
+     {backgroundColor:"#D7DBDD", color: "#2E4053"},
      {backgroundColor:"#2E86C1 ", color: "#F7F9F9"},
-     {backgroundColor:"#CB4335", color: "#F7F9F9"},
-
+     {backgroundColor:"#CB4335", color: "#F7F9F9"}
   ]
-  
+  currentTheme:ThemeProps = this.themeStyles[0]
 }
 
