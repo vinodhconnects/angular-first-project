1660748fc68d41952970b0ed025eb0c346eee338
vinodhconnects <vinodhconnects@gmail.com>
2023-06-07 11:13:15 +0530

    input-complete

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { ThemeProps } from '../types';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+   @Input("ftheme") footertheme:ThemeProps= {backgroundColor:"",color:""}
 }
